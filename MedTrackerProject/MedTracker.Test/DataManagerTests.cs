905561c8394a16c8f56b03c600bdfd50078b9b7d
Maverick Wagley <103015775+maverickwagley@users.noreply.github.com>
2026-04-01 02:37:50 +0000

    chore: add datamanager tests

--- MedTrackerProject/MedTracker.Test/DataManagerTests.cs
+++ MedTrackerProject/MedTracker.Test/DataManagerTests.cs
@@ -14,4 +14,18 @@ public class DataManagerTests
         var _name = testDM.Username;
         Assert.Equal("New User",_name);
     }
+    [Fact]
+    public void Test_AddMed()
+    {
+        testDM.AddMedication("med2");
+        var mName = testDM.Meds[0].Name;
+        Assert.Equal("med2", mName);
+    }
+    [Fact]
+    public void Test_RemoveMed()
+    {
+        testDM.RemoveMedication("med2");
+        int mCount = testDM.Meds.Count;
+        Assert.Equal(0, mCount);
+    }
 }
